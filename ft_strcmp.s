7b29c33d527a11091e590ada1b28334a0b8293da
Adrien Fernandes <adda-sil@student.42.fr>
2020-03-07 21:23:53 +0100

    Update ft_strcmp.s

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -1,31 +1,18 @@
 global _ft_strcmp
-
 section .text
-
-_ft_strcmp: ;rdi rsi
-	mov rax, 0
-
+_ft_strcmp:
+		mov rax, 0
+		mov r15, 0
 .loop:
-	mov bl, [rsi]
-	mov bh, [rdi]
-	cmp bl, bh
-	jg .sup
-	jl .inf
-	cmp bl, 0
-	je .end
-	cmp bh, 0
-	je .end
-	inc rdi
-	inc rsi
-	jmp .loop
-
-.sup:
-	mov rax, -1
-	ret
-
-.inf:
-	mov rax, 1
-	ret
-
+		mov		al, [rdi]
+		mov		r15b, [rsi]
+		cmp		al, r15b
+		jne		.end
+		cmp		al, 0
+		je		.end
+		inc		rsi
+		inc		rdi
+		jmp		.loop
 .end:
-	ret
+		sub rax, r15
+		ret
